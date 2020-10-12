80258827cba2c7c213700368609a86bcfc5cd1b2
katyamishha <katyamishha@gmail.com>
2020-10-12 15:27:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('');
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketsConfig[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+  }
+
+  let i = 0;
+  while (i < strArr.length) { 
+    if (bracketsConfig.includes(strArr[i] + strArr[i + 1])) {
+      strArr.splice(i, 2);
+      i = 0;
+    }else i++;
+  }
+
+  return strArr.length == 0;
 }
